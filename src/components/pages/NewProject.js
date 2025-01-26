035877595ddbda3d1929b370350b7ba8ab7178d2
Rodolfo <126685464+Rodolfo12ads@users.noreply.github.com>
2025-01-26 18:18:30 -0300

    Update NewProject.js

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -10,7 +10,7 @@ function NewProject() {
         project.cost = 0;
         project.services = [];
 
-        fetch('http://localhost:5000/projects', {
+        fetch('https://costs-production-d20b.up.railway.app/projects', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
